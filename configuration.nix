f62b8e458e0c266e2d196b1a6ee0d075e624710b
GrannySmith21 <bastien.barbe65@gmail.com>
2024-02-12 16:48:14 +0100

    Initial commit.

---
+++ configuration.nix
@@ -0,0 +1,264 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+let
+  # EFI mount point :
+  EFI_MOUNTPOINT = "/boot";
+  # user name
+  USER_NAME = "smith";
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # ------------------------------------------
+  # Bootloader
+  # ------------------------------------------
+
+  # Use GRUB as bootloader
+  boot.loader = {
+    timeout = 30;
+    grub = {
+      minegrub-theme = {
+        enable = true;
+        splash = "Per Aspera Ad Astra";
+      };
+      enable = true;
+      efiSupport = true;
+      efiInstallAsRemovable = false; # set to true if needed
+      device = "nodev";
+      useOSProber = true;
+      extraEntries = ''
+        menuentry "Reboot" {
+          reboot
+        }
+        menuentry "Poweroff" {
+          halt
+        }
+      '';
+    };
+    efi = {
+      efiSysMountPoint = "${EFI_MOUNTPOINT}"; # adjust if your mount point differs
+      canTouchEfiVariables = true;
+    };
+  };
+
+  networking.hostName = "TENVEL9"; # Define your hostname.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # bluetooth
+  hardware.bluetooth.enable = true;
+
+  # Experimental features
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Set your time zone.
+  time.timeZone = "Europe/Paris";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fr_FR.UTF-8";
+    LC_IDENTIFICATION = "fr_FR.UTF-8";
+    LC_MEASUREMENT = "fr_FR.UTF-8";
+    LC_MONETARY = "fr_FR.UTF-8";
+    LC_NAME = "fr_FR.UTF-8";
+    LC_NUMERIC = "fr_FR.UTF-8";
+    LC_PAPER = "fr_FR.UTF-8";
+    LC_TELEPHONE = "fr_FR.UTF-8";
+    LC_TIME = "fr_FR.UTF-8";
+  };
+
+  # Configure console keymap
+  console.keyMap = "fr";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Fonts
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = [
+      # Names of fonts to use:
+      # -Tinos: Tinos
+      # -FiraCode: Fira Code
+      # -JetBrainsMono: 'JetBrainsMono Nerd Font'
+      "FiraCode" "JetBrainsMono" "Tinos"
+    ]; })
+  ];
+
+  # ------------------------------------------
+  # GUI
+  # ------------------------------------------
+
+  # Enable the X11 windowing system with wayland support
+  services.xserver = {
+    enable = true;
+    displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
+    };
+    desktopManager.plasma5.enable = true;
+    videoDrivers = ["nvidia"]; # Load nvidia driver for Xorg and Wayland
+    xkb = {
+      layout = "fr";
+      variant = "";
+    }; # Configure keymap in X11
+  };
+
+  # Enable OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  hardware.nvidia = {
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    powerManagement.enable = false;
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+	  # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # NVIDIA driver (select appropriate package for your card)
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    # Enable Optimus Prime support
+    prime = {
+      sync.enable = true;
+      
+      # $ sudo lshw -c display
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+
+
+
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.smith = {
+    isNormalUser = true;
+    description = "smith";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      firefox
+      kate
+      vscode.fhs
+      brave
+      zotero
+      discord
+      libreoffice
+      thunderbird
+
+      # utilities
+      pavucontrol
+      networkmanagerapplet
+      brightnessctl
+      kitty # terminal
+      blueberry # bluetooth manager
+      stacer # system monitor
+      libsForQt5.kamoso # camera utility
+      ventoy-full # makebootable usb
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    gedit
+    git
+    direnv
+    neofetch
+    lshw # for hardware information
+    htop
+    tree
+    openssl
+    usbutils
+    appimage-run
+    gparted
+    python3 # for scripting (add no packages here, use dev shell instead)
+    sshpass
+    openvpn # VPN
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
